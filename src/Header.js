a8f69924cfd44cedfb31c57ae9612d95fedf6d15
IMRANDIL <aliimranadil2@gmail.com>
2022-03-10 21:01:28 +0530

    yh

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,25 @@
 import React from 'react'
-
+import SearchIcon from '@mui/icons-material/Search';
+import LinkedIn from '../src/images/linkedin.svg';
 import './Header.css'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import WorkIcon from '@mui/icons-material/Work';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
 
 
 const Header = () => {
     return (
         <div className='header'>
             <div className="header__left">
-                <img src="" alt="" />
+                <img src={LinkedIn} alt="linkedin-img" />
 
                 <div className="header__search">
-                    {/* materialui search icon */}
+                    <SearchIcon />
                     <input type="text" />
                 </div>
 
@@ -19,7 +28,12 @@ const Header = () => {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title='Home' />
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+                <HeaderOption Icon={WorkIcon} title='Jobs' />
+                <HeaderOption Icon={ChatIcon} title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOption avatar='https://avatars.githubusercontent.com/u/71559091?s=400&u=261c7198a75f868c1d0c5deda860c96ad06d8820&v=4' title='me' />
 
             </div>
 
