356e52aac3b23c56555629c4829f0217116c8a81
IMRANDIL <aliimranadil2@gmail.com>
2022-03-10 19:15:52 +0530

    yh

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header__left">
+                <img src="" alt="" />
+
+                <div className="header__search">
+                    {/* materialui search icon */}
+                    <input type="text" />
+                </div>
+
+
+
+            </div>
+
+            <div className="header__right">
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
