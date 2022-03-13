d039efcdacc729630350555ca066a254f8e4cd95
IMRANDIL <aliimranadil2@gmail.com>
2022-03-13 14:02:17 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useSelector } from 'react-redux'
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react';
 
 
 import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import { selectUser } from './features/userSlice';
+import { login, logout, selectUser } from './features/userSlice';
 import Login from './Login';
+import { auth } from './firebase';
 
 
 
@@ -18,6 +19,32 @@ import Login from './Login';
 function App() {
 
   const user = useSelector(selectUser)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        //user is logged in...
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL
+        }))
+
+      } else {
+        //user is logged out...
+        dispatch(logout())
+      }
+    })
+
+
+
+  }, [dispatch])
+
+
 
 
 
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'
 import LinkedIn from '../src/images/img21.png';
 import { auth } from './firebase';
-import './Login.css'
+import './Login.css';
+import { login } from './features/userSlice';
 
 
 
@@ -20,19 +22,35 @@ const Login = () => {
     const [profilePic, setProfilePic] = useState('')
 
 
-
+    const dispatch = useDispatch()
 
 
 
 
     const register = () => {
+        if (!name) {
+            return alert('Please Enter a Full Name')
+        }
 
+        auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic
+            }).then(() => {
+                dispatch(login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoURL: profilePic
+                }))
+            })
+        }).catch((err) => alert(err))
 
 
     }
 
 
-    const login = (e) => {
+    const handleLogin = (e) => {
         e.preventDefault()
 
 
@@ -51,8 +69,8 @@ const Login = () => {
                 <input type="text" placeholder='Full name (required if registering)' value={name} onChange={e => setName(e.target.value)} />
                 <input type="text" placeholder='Profile picUrl (optional)' value={profilePic} onChange={e => setProfilePic(e.target.value)} />
                 <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder='Password' value={password} onChange={(e) => setEmail(e.target.value)} />
-                <button type='submit' onClick={login}>Sign In</button>
+                <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type='submit' onClick={handleLogin}>Sign In</button>
             </form>
 
             <p>Not a member?{" "}
